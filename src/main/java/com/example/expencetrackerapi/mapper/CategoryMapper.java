767fd3baf36dc2a073194d8e9ba51719d2e92fcf
TheGlitch26 <nihad.bagirzade888@gmail.com>
2026-05-13 18:36:52 +0400

    feat: added Category flow

---
+++ src/main/java/com/example/expencetrackerapi/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package com.example.expencetrackerapi.mapper;
+
+
+import com.example.expencetrackerapi.dto.request.CategoryResponse;
+import com.example.expencetrackerapi.entity.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public CategoryResponse toResponse(Category category){
+        CategoryResponse response = new CategoryResponse();
+        response.setId(category.getId());
+        response.setName(category.getName());
+        response.setDescription(category.getDescription());
+        response.setAccountId(category.getAccount().getId());
+
+        return response;
+    }
+}
