97d5921fc765eec59db8f760989b2890cecd0b8c
TheGlitch26 <nihad.bagirzade888@gmail.com>
2026-05-14 16:39:09 +0400

    feat: added category flow

--- src/main/java/com/example/expencetrackerapi/controller/CategoryController.java
+++ src/main/java/com/example/expencetrackerapi/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.example.expencetrackerapi.controller;
 
 
+import com.example.expencetrackerapi.dto.request.CategoryRequest;
+import com.example.expencetrackerapi.dto.response.CategoryResponse;
 import com.example.expencetrackerapi.service.CategoryService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -14,5 +20,30 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @GetMapping
+    public List<CategoryResponse> findAll(){
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse findById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(request));
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponse update(@PathVariable Long id, @RequestBody CategoryRequest request){
+        return categoryService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        categoryService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
--- src/main/java/com/example/expencetrackerapi/service/CategoryService.java
+++ src/main/java/com/example/expencetrackerapi/service/CategoryService.java
@@ -1,14 +1,13 @@
 package com.example.expencetrackerapi.service;
 
 import com.example.expencetrackerapi.dto.request.CategoryRequest;
-import com.example.expencetrackerapi.dto.request.CategoryResponse;
+import com.example.expencetrackerapi.dto.response.CategoryResponse;
 import com.example.expencetrackerapi.entity.Account;
 import com.example.expencetrackerapi.entity.Category;
 import com.example.expencetrackerapi.exception.ResourceNotFoundException;
 import com.example.expencetrackerapi.mapper.CategoryMapper;
+import com.example.expencetrackerapi.repository.AccountRepository;
 import com.example.expencetrackerapi.repository.CategoryRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,11 +18,13 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final AccountService accountService;
     private final CategoryMapper categoryMapper;
+    private final AccountRepository accountRepository;
 
-    public CategoryService(CategoryRepository categoryRepository, AccountService accountService, CategoryMapper categoryMapper) {
+    public CategoryService(CategoryRepository categoryRepository, AccountService accountService, CategoryMapper categoryMapper, AccountRepository accountRepository) {
         this.categoryRepository = categoryRepository;
         this.accountService = accountService;
         this.categoryMapper = categoryMapper;
+        this.accountRepository = accountRepository;
     }
 
     public List<CategoryResponse> findAll(){
@@ -37,20 +38,48 @@ public class CategoryService {
         return responses;
     }
 
-    public Category findById(Long id){
-        return categoryRepository.findById(id)
+    public CategoryResponse findById(Long id){
+        Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Category with this is id is not found: " + id));
+        return categoryMapper.toResponse(category);
     }
 
-    public ResponseEntity<Category> create(CategoryRequest request){
-        Account account = accountService.findById(request.getAccountId());
+    public CategoryResponse create(CategoryRequest request){
+        Account account = accountRepository.findById(request.getAccountId())
+                .orElseThrow(() -> new ResourceNotFoundException("Account not found with id: " + request.getAccountId()));
 
         Category category = new Category();
         category.setName(request.getName());
         category.setAccount(account);
         category.setDescription(request.getDescription());
-        return ResponseEntity.status(HttpStatus.CREATED).body(categoryRepository.save(category));
+        return categoryMapper.toResponse(categoryRepository.save(category));
     }
 
+    public CategoryResponse update(Long id, CategoryRequest request){
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
 
+        if(request.getAccountId() != null){
+            Account account = accountRepository.findById(request.getAccountId())
+                            .orElseThrow(() -> new ResourceNotFoundException("Account not found with id: " + request.getAccountId()));
+
+            category.setAccount(account);
+        }
+        if(request.getName() != null && !request.getName().isBlank()){
+            category.setName(request.getName());
+        }
+        if(request.getDescription() != null && !request.getDescription().isBlank()){
+            category.setDescription(request.getDescription());
+        }
+
+        categoryRepository.save(category);
+        return categoryMapper.toResponse(category);
+    }
+
+    public void delete(Long id){
+        Category category = categoryRepository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("Category not found with id: " + id));
+
+        categoryRepository.delete(category);
+    }
 }
